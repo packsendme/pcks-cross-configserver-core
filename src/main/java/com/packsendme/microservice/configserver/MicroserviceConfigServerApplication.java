fc7c5d4c852b7c826e5ac0d70715e596b1782b07
packsendme <packsendme@gmail.com>
2018-07-17 13:39:27 +0200

    initial-version-microservice

---
+++ src/main/java/com/packsendme/microservice/configserver/MicroserviceConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.packsendme.microservice.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConfigServerApplication.class, args);
+	}
+}
